ffc4f871f86900c8b443b05a2367368f94adaca9
Gabriel Fernandes <defren.gabriel@gmail.com>
2025-02-14 16:57:18 -0300

    chave api do projeto foi ocultada

--- src/Firebase/config.jsx
+++ src/Firebase/config.jsx
@@ -3,19 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDn-j_vnVpbKX1ST_z8KJthrn19UXXxPls",
-  authDomain: "aquela-compra.firebaseapp.com",
-  projectId: "aquela-compra",
-  storageBucket: "aquela-compra.firebasestorage.app",
-  messagingSenderId: "837975569108",
-  appId: "1:837975569108:web:ece04de4b5dfe2fa1bc141",
-  measurementId: "G-RN3PKHC4XN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
