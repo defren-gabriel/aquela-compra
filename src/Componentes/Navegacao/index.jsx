1f19b741aaa8038f01c3afb001c3ed736061a08c
Gabriel Fernandes <defren.gabriel@gmail.com>
2025-02-15 10:32:39 -0300

    deploy

--- src/Componentes/Navegacao/index.jsx
+++ src/Componentes/Navegacao/index.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 import { useAuth } from "../../Contextos/AuthLoginLogout";
